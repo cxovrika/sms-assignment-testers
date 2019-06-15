5d8211c7239eb85c6e9152be55f2f5dc403a35f9
cerushdope <george.tsertsvadze97@gmail.com>
2019-06-16 00:23:12 +0400

    Add hangman test

---
+++ HangmanTest/commands.bat
@@ -0,0 +1,6 @@
+copy HangmanTest.java .\work\Assignment4\
+copy acm.jar .\work\Assignment4\
+javac -cp .\work\Assignment4\acm.jar .\work\Assignment4\HangmanTest.java .\work\Assignment4\Hangman.java .\work\Assignment4\HangmanCanvas.java .\work\Assignment4\HangmanLexicon.java
+cd work
+cd Assignment4
+java -cp ".\;.\acm.jar" HangmanTest
