a18c43f82a487e468158665e14943be18214c947
cerushdope <george.tsertsvadze97@gmail.com>
2019-06-22 01:10:13 +0400

    Change relative path to full path

--- KarelTest/commands.bat
+++ KarelTest/commands.bat
@@ -11,4 +11,4 @@ copy %1\acm.jar %1\work\Assignment1\
 cd work
 cd Assignment1
 javac -cp %1\work\Assignment1\karel.jar;%1\work\Assignment1\junit-4.12.jar;%1\work\Assignment1\hamcrest-core-1.3.jar;%1\work\Assignment1\acm.jar %1\work\Assignment1\CollectNewspaperKarel.java %1\work\Assignment1\StoneMasonKarel.java %1\work\Assignment1\CheckerBoardKarel.java %1\work\Assignment1\MidpointFindingKarel.java %1\work\Assignment1\KarelTester.java %1\work\Assignment1\CollectNewspaperKarelTest.java %1\work\Assignment1\StoneMasonKarelTest.java %1\work\Assignment1\CheckerBoardKarelTest.java %1\work\Assignment1\MidpointFindingKarelTest.java
-java -cp .;karel.jar;junit-4.12.jar;hamcrest-core-1.3.jar;acm.jar KarelTester
+java -cp "%1\work\Assignment1;%1\work\Assignment1\karel.jar;%1\work\Assignment1\junit-4.12.jar;%1\work\Assignment1\hamcrest-core-1.3.jar;%1\work\Assignment1\acm.jar" KarelTester > %1\output.txt
