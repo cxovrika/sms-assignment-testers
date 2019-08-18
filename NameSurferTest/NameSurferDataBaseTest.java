3f93903078aa152628dac6b6073efe809438f9dc
ekavt15 <ekavt15@freeuni.edu.ge>
2019-08-18 15:22:35 +0400

    Update failure messages

--- NameSurferTest/NameSurferDataBaseTest.java
+++ NameSurferTest/NameSurferDataBaseTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -7,44 +8,44 @@ public class NameSurferDataBaseTest {
 	@Test
 	public void testFindEntry() {
 		NameSurferDataBase db = new NameSurferDataBase("names-data-small.txt");
-		assertEquals(true, (db.findEntry("A")).getName().equals("A"));
-		assertEquals(true, (db.findEntry("Aylin")).getName().equals("Aylin"));
-		assertEquals(true, db.findEntry("Baby").getName().equals("Baby"));
-		assertEquals(true, db.findEntry("Coby").getName().equals("Coby"));
-		assertEquals(true, db.findEntry("Cyrus").getName().equals("Cyrus"));
-		assertEquals(true, db.findEntry("Ezra").getName().equals("Ezra"));
-		assertEquals(true, db.findEntry("Fabian").getName().equals("Fabian"));
-		assertEquals(true, db.findEntry("Dagmar").getName().equals("Dagmar"));
-		assertEquals(true, db.findEntry("Darrin").getName().equals("Darrin"));
-		assertEquals(true, db.findEntry("Tyreek").getName().equals("Tyreek"));
-		assertEquals(true, db.findEntry("Zulma").getName().equals("Zulma"));
+		assertTrue("Entry was not found in database", db.findEntry("A").getName().equals("A"));
+		assertTrue("Entry was not found in database", db.findEntry("Aylin").getName().equals("Aylin"));
+		assertTrue("Entry was not found in database", db.findEntry("Baby").getName().equals("Baby"));
+		assertTrue("Entry was not found in database", db.findEntry("Coby").getName().equals("Coby"));
+		assertTrue("Entry was not found in database", db.findEntry("Cyrus").getName().equals("Cyrus"));
+		assertTrue("Entry was not found in database", db.findEntry("Ezra").getName().equals("Ezra"));
+		assertTrue("Entry was not found in database", db.findEntry("Fabian").getName().equals("Fabian"));
+		assertTrue("Entry was not found in database", db.findEntry("Dagmar").getName().equals("Dagmar"));
+		assertTrue("Entry was not found in database", db.findEntry("Darrin").getName().equals("Darrin"));
+		assertTrue("Entry was not found in database", db.findEntry("Tyreek").getName().equals("Tyreek"));
+		assertTrue("Entry was not found in database", db.findEntry("Zulma").getName().equals("Zulma"));
 	}	
 	
 	@Test
 	public void testFindEntryNonExisting() {
 		NameSurferDataBase db = new NameSurferDataBase("names-data-small.txt");
-		assertEquals(null, db.findEntry("Bla"));
-		assertEquals(null, db.findEntry("Elene"));
-		assertEquals(null, db.findEntry("Giorgi"));
-		assertEquals(null, db.findEntry("Aleksandre"));
+		assertTrue("Extra data was found in database", null == db.findEntry("Bla"));
+		assertTrue("Extra data was found in database", null == db.findEntry("Elene"));
+		assertTrue("Extra data was found in database", null == db.findEntry("Giorgi"));
+		assertTrue("Extra data was found in database", null == db.findEntry("Aleksandre"));
 	}	
 	
 	@Test
 	public void testFindEntrySingleData() {
 		NameSurferDataBase db = new NameSurferDataBase("names-data-small.txt");
 		NameSurferEntry entry = db.findEntry("A");
-		assertEquals(true, entry.getName().equals("A"));
-		assertEquals(83, entry.getRank(0));
-		assertEquals(140, entry.getRank(1));
-		assertEquals(228, entry.getRank(2));
-		assertEquals(286, entry.getRank(3));
-		assertEquals(426, entry.getRank(4));
-		assertEquals(612, entry.getRank(5));
-		assertEquals(486, entry.getRank(6));
-		assertEquals(577, entry.getRank(7));
-		assertEquals(836, entry.getRank(8));
-		assertEquals(0, entry.getRank(9));
-		assertEquals(0, entry.getRank(10));
+		assertTrue("Name for entry was not correct", true == entry.getName().equals("A"));
+		assertTrue("Rank for entry was not correct", 83 == entry.getRank(0));
+		assertTrue("Rank for entry was not correct", 140 == entry.getRank(1));
+		assertTrue("Rank for entry was not correct", 228 == entry.getRank(2));
+		assertTrue("Rank for entry was not correct", 286 == entry.getRank(3));
+		assertTrue("Rank for entry was not correct", 426 == entry.getRank(4));
+		assertTrue("Rank for entry was not correct", 612 == entry.getRank(5));
+		assertTrue("Rank for entry was not correct", 486 == entry.getRank(6));
+		assertTrue("Rank for entry was not correct", 577 == entry.getRank(7));
+		assertTrue("Rank for entry was not correct", 836 == entry.getRank(8));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(9));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(10));
 	}	
 	
 	@Test
@@ -52,32 +53,32 @@ public class NameSurferDataBaseTest {
 		NameSurferDataBase db = new NameSurferDataBase("names-data-small.txt");
 		
 		NameSurferEntry entry = db.findEntry("A");
-		assertEquals(true, entry.getName().equals("A"));
-		assertEquals(83, entry.getRank(0));
-		assertEquals(140, entry.getRank(1));
-		assertEquals(228, entry.getRank(2));
-		assertEquals(286, entry.getRank(3));
-		assertEquals(426, entry.getRank(4));
-		assertEquals(612, entry.getRank(5));
-		assertEquals(486, entry.getRank(6));
-		assertEquals(577, entry.getRank(7));
-		assertEquals(836, entry.getRank(8));
-		assertEquals(0, entry.getRank(9));
-		assertEquals(0, entry.getRank(10));
+		assertTrue("Name for entry was not correct", true == entry.getName().equals("A"));
+		assertTrue("Rank for entry was not correct", 83 == entry.getRank(0));
+		assertTrue("Rank for entry was not correct", 140 == entry.getRank(1));
+		assertTrue("Rank for entry was not correct", 228 == entry.getRank(2));
+		assertTrue("Rank for entry was not correct", 286 == entry.getRank(3));
+		assertTrue("Rank for entry was not correct", 426 == entry.getRank(4));
+		assertTrue("Rank for entry was not correct", 612 == entry.getRank(5));
+		assertTrue("Rank for entry was not correct", 486 == entry.getRank(6));
+		assertTrue("Rank for entry was not correct", 577 == entry.getRank(7));
+		assertTrue("Rank for entry was not correct", 836 == entry.getRank(8));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(9));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(10));
 		
 		entry = db.findEntry("Aylin");
-		assertEquals(true, entry.getName().equals("Aylin"));
-		assertEquals(0, entry.getRank(0));
-		assertEquals(0, entry.getRank(1));
-		assertEquals(0, entry.getRank(2));
-		assertEquals(0, entry.getRank(3));
-		assertEquals(0, entry.getRank(4));
-		assertEquals(0, entry.getRank(5));
-		assertEquals(0, entry.getRank(6));
-		assertEquals(0, entry.getRank(7));
-		assertEquals(0, entry.getRank(8));
-		assertEquals(0, entry.getRank(9));
-		assertEquals(728, entry.getRank(10));
+		assertTrue("Name for entry was not correct", true == entry.getName().equals("Aylin"));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(0));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(1));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(2));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(3));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(4));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(5));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(6));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(7));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(8));
+		assertTrue("Rank for entry was not correct", 0 == entry.getRank(9));
+		assertTrue("Rank for entry was not correct", 728 == entry.getRank(10));
 	}	
 	
 }
--- NameSurferTest/NameSurferEntryTest.java
+++ NameSurferTest/NameSurferEntryTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -7,35 +8,35 @@ public class NameSurferEntryTest {
 	public void testSimple() {
 		String line = "A 83 140 228 286 426 612 486 577 836 0 0";
 		NameSurferEntry entry = new NameSurferEntry(line);
-		assertEquals(entry.getName(), "A");
-		assertEquals(entry.getRank(0), 83);
-		assertEquals(entry.getRank(1), 140);
-		assertEquals(entry.getRank(2), 228);
-		assertEquals(entry.getRank(3), 286);
-		assertEquals(entry.getRank(4), 426);
-		assertEquals(entry.getRank(5), 612);
-		assertEquals(entry.getRank(6), 486);
-		assertEquals(entry.getRank(7), 577);
-		assertEquals(entry.getRank(8), 836);
-		assertEquals(entry.getRank(9), 0);
-		assertEquals(entry.getRank(10), 0);
+		assertTrue("Returned name was not correct", entry.getName().equals("A"));
+		assertTrue("Rank was not correct", entry.getRank(0) == 83);
+		assertTrue("Rank was not correct", entry.getRank(1) == 140);
+		assertTrue("Rank was not correct", entry.getRank(2) == 228);
+		assertTrue("Rank was not correct", entry.getRank(3) == 286);
+		assertTrue("Rank was not correct", entry.getRank(4) == 426);
+		assertTrue("Rank was not correct", entry.getRank(5) == 612);
+		assertTrue("Rank was not correct", entry.getRank(6) == 486);
+		assertTrue("Rank was not correct", entry.getRank(7) == 577);
+		assertTrue("Rank was not correct", entry.getRank(8) == 836);
+		assertTrue("Rank was not correct", entry.getRank(9) == 0);
+		assertTrue("Rank was not correct", entry.getRank(10) == 0);
 	}
 	
 	@Test
 	public void testAllZero() {
 		String line = "A 0 0 0 0 0 0 0 0 0 0 0";
 		NameSurferEntry entry = new NameSurferEntry(line);
-		assertEquals(0, entry.getRank(0));
-		assertEquals(0, entry.getRank(1));
-		assertEquals(0, entry.getRank(2));
-		assertEquals(0, entry.getRank(3));
-		assertEquals(0, entry.getRank(4));
-		assertEquals(0, entry.getRank(5));
-		assertEquals(0, entry.getRank(6));
-		assertEquals(0, entry.getRank(7));
-		assertEquals(0, entry.getRank(8));
-		assertEquals(0, entry.getRank(9));
-		assertEquals(0, entry.getRank(10));
+		assertTrue("Rank was not correct", 0 == entry.getRank(0));
+		assertTrue("Rank was not correct", 0 == entry.getRank(1));
+		assertTrue("Rank was not correct", 0 == entry.getRank(2));
+		assertTrue("Rank was not correct", 0 == entry.getRank(3));
+		assertTrue("Rank was not correct", 0 == entry.getRank(4));
+		assertTrue("Rank was not correct", 0 == entry.getRank(5));
+		assertTrue("Rank was not correct", 0 == entry.getRank(6));
+		assertTrue("Rank was not correct", 0 == entry.getRank(7));
+		assertTrue("Rank was not correct", 0 == entry.getRank(8));
+		assertTrue("Rank was not correct", 0 == entry.getRank(9));
+		assertTrue("Rank was not correct", 0 == entry.getRank(10));
 	}
 	
 }
